e2e714bfeb5ecd030af76c538be7b413b5fccc84
Asif Sayyed <asifdotexe@gmail.com>
2024-07-05 23:45:40 +0530

    #18 created add_feature function

--- code/strendforcast.py
+++ code/strendforcast.py
@@ -38,4 +38,35 @@ def fetch_data(ticker: str,
         plt.savefig(f'plots/{ticker}_closing_price.png')
         plt.close()
     return data
+
+def add_features(data: pd.DataFrame, *horizons) -> pd.DataFrame:
+    """
+    Adds features to the input DataFrame for stock price prediction.
     
+    Parameters:
+    - data (pd.DataFrame): A DataFrame containing historical stock data.
+    
+    Returns:
+    - pd.DataFrame: A DataFrame with added features for stock price prediction.
+    
+    The function adds the following features to the input DataFrame:
+    1. 'tomorrow': The closing price of the next day.
+    2. 'target': A binary indicator variable that takes the value 1 if the closing price of the next day is higher than the current closing price, and 0 otherwise.
+    3. For each horizon in the list of horizons (2, 5, 60, 250, 1000), it calculates the rolling average of the closing price over that horizon and divides it by the current closing price to create a new feature with the name '{horizon}_day'.
+
+    The function then drops any remaining columns from the DataFrame.
+    """
+    data['tomorrow'] = data['close'].shift(-1)
+    data['target'] = (data['tomorrow'] > data['close']).astype(int)
+    """ Horizon logic
+     2 -> two days
+     5 -> one week
+     60 -> three months
+     1000 -> 4 years
+     """
+    horizons = [2, 5, 60, 250, 1000]
+    for horizon in horizons:
+        rolling_average = data['close'].rolling(horizon).mean()
+        data[f'{horizon}_day'] = rolling_average / data['close']
+    data = data.drop()
+    return data
